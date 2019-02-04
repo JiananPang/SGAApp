036d74df44b81f4af3ec8633e0b729cf2f32cf90
xix1 <emperorstark1@gmail.com>
2019-02-04 12:58:09 -0500

    rosefire in progress

--- app/src/main/java/rose/pangj/sga_app/LogInFragment.kt
+++ app/src/main/java/rose/pangj/sga_app/LogInFragment.kt
@@ -1,17 +1,43 @@
 package rose.pangj.sga_app
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.log_in.view.*
 
 class LogInFragment : Fragment() {
+
+    var listener: OnLoginButtonPressedListener? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.log_in, container, false)
+        val view = inflater.inflate(R.layout.log_in, container, false)
+        view.log_in_button.setOnClickListener {
+            listener?.onLoginButtonPressed()
+        }
+        return view
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnLoginButtonPressedListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnLoginButtonPressedListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    interface OnLoginButtonPressedListener {
+        fun onLoginButtonPressed()
     }
 }
--- app/src/main/java/rose/pangj/sga_app/MainActivity.kt
+++ app/src/main/java/rose/pangj/sga_app/MainActivity.kt
@@ -9,18 +9,50 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener,
-    DocumentsFragment.OnDocSelectedListener, NewsFragment.OnNewsSelectedListener {
+    DocumentsFragment.OnDocSelectedListener, NewsFragment.OnNewsSelectedListener, LogInFragment.OnLoginButtonPressedListener {
+
     val mNewsFragment = NewsFragment()
+    val auth = FirebaseAuth.getInstance()
+    lateinit var authListener: FirebaseAuth.AuthStateListener
+    private val RC_ROSEFIRE_LOGIN = 1
+
+    override fun onStart() {
+        super.onStart()
+        auth.addAuthStateListener(authListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        auth.removeAuthStateListener(authListener)
+    }
+
+    private fun initializeListeners() {
+        authListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
+            val user = auth.currentUser
+
+            if(user == null){
+                fragmentTransaction(LogInFragment())
+            }else {
+                fragmentTransaction(NewsFragment())
+            }
+        }
+    }
+
+    override fun onLoginButtonPressed() {
+//        val signInIntent = Rosefire.getSignInIntent(this,"bf999ecb-f11b-4d0d-88af-a1860eedaea3")
+//        startActivityForResult(signInIntent, RC_ROSEFIRE_LOGIN)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        fragmentTransaction(LogInFragment())
         setSupportActionBar(toolbar)
+        initializeListeners()
 
 
         val toggle = ActionBarDrawerToggle(
